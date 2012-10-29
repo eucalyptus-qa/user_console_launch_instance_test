26285c7fcb3a12acd3377769e69ce0e5b35ab493
Kyo Lee <kyo.lee@eucalyptus.com>
2012-10-29 16:34:01 -0700

    First Commmit

---
+++ stage04/check_deleted_keypairs.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+
+print "\n";
+print "RUN sel_get_dashboard_test.pl\n";
+print "\n";
+
+my $dashboard_view = `perl ./sel_get_dashboard_test.pl`;
+
+print "\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "\n";
+
+print $dashboard_view . "\n";
+print "\n";
+
+print "\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "\n";
+
+print "\n";
+print "\n";
+print "\n";
+
+print "DASH BOARD VIEW SUMMARY\n";
+
+print "\n";
+print "\n";
+
+my @lines = split("\n", $dashboard_view);
+
+foreach my $line (@lines){
+	chomp($line);
+	if( $line =~ /^ACCOUNTNAME/ ){
+		print "\n";
+		print $line . "\n";
+	}elsif( $line =~ /^USERNAME/ ){
+		print $line . "\n";
+	}elsif( $line =~ /^\[DASHBOARD\]/ ){
+		print $line . "\n";
+		check_dashboard_element_for_deleted_keypair($line);
+	};	
+};
+
+
+
+exit(0);
+
+1;
+
+
+################# SUBROUTINE ########################
+
+sub check_dashboard_element_for_deleted_keypair{
+	my $line = shift @_;
+
+	if( $line =~ /^\[DASHBOARD\]\s+Keypairs:\s+(\d+)/ ){
+		if( $1 != 0 ){
+			print "[TEST_RESULT]\tFAILED: TO DELETE KEYPAIR VIA USER CONSOLE !!\n";
+		};
+	};
+	return;
+};
+
+1;
+
+
+
+
+
