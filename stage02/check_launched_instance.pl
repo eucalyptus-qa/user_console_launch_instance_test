29f7aebe1fe1126692187daa574f23c019ad5349
Kyo Lee <kyo.lee@eucalyptus.com>
2012-10-29 21:16:54 -0700

    First Commit

---
+++ stage02/check_launched_instance.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+
+print "\n";
+print "RUN sel_get_dashboard_test.pl\n";
+print "\n";
+
+my $dashboard_view = `perl ./sel_get_dashboard_test_reduced.pl`;
+
+print "\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "\n";
+
+print $dashboard_view . "\n";
+print "\n";
+
+print "\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "===============================================================================================\n";
+print "\n";
+
+print "\n";
+print "\n";
+print "\n";
+
+print "DASH BOARD VIEW SUMMARY\n";
+
+print "\n";
+print "\n";
+
+my @lines = split("\n", $dashboard_view);
+
+foreach my $line (@lines){
+	chomp($line);
+	if( $line =~ /^ACCOUNTNAME/ ){
+		print "\n";
+		print $line . "\n";
+	}elsif( $line =~ /^USERNAME/ ){
+		print $line . "\n";
+	}elsif( $line =~ /^\[DASHBOARD\]/ ){
+		print $line . "\n";
+		check_dashboard_element_for_launched_instance($line);
+	};	
+};
+
+
+
+exit(0);
+
+1;
+
+
+################# SUBROUTINE ########################
+
+sub check_dashboard_element_for_launched_instance{
+	my $line = shift @_;
+
+	if( $line =~ /^\[DASHBOARD\]\s+Running Instances:\s+(\d+)/ ){
+		if( $1 != 1 ){
+			print "[TEST_RESULT]\tFAILED: TO LAUNCH INSTANCE VIA USER CONSOLE !!\n";
+		};
+	};
+	return;
+};
+
+1;
+
+
+
+
+
